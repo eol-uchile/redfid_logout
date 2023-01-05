0fc25639fdb995ab66dbfad67bafe6b1c2b37593
Luis Santana <luis.santana101@gmail.com>
2023-01-05 17:17:16 -0300

    Update urls.py

--- redfid_logout/urls.py
+++ redfid_logout/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.conf.urls import url
+from django.views.decorators.csrf import csrf_exempt
 from .views import *
 
 
 urlpatterns = [
     url('logout_get/', RedfidLogoutGet.as_view(), name='logout_get'),
-    url('logout_post/', RedfidLogoutPost.as_view(), name='logout_post'),
+    url('logout_post/', csrf_exempt(RedfidLogoutPost.as_view()), name='logout_post'),
 ]
